6e196ac8ff0f3c19928a03152ff601951e857edf
Enock <bernardo.enock@vileve.com.br>
2025-09-05 20:51:28 -0300

    feat: Create UserRepository

---
+++ argonock/src/main/java/com/api/argonock/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.api.argonock.repository;
+
+import com.api.argonock.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<User, String> {
+  UserDetails findByEmail(String email);
+}
