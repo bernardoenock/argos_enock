ede2f8c025291f45d8463123aee1ced283152643
Enock <bernardo.enock@vileve.com.br>
2025-09-06 20:06:43 -0300

    feat: Create CommentRepository

---
+++ argonock/src/main/java/com/api/argonock/repository/CommentRepository.java
@@ -0,0 +1,13 @@
+package com.api.argonock.repository;
+
+import com.api.argonock.model.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+  List<Comment> findByPostId(Long postId);
+}
