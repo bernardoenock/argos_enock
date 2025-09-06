b9ff82218b8b34f260d229607f74bc0643b385a0
Enock <bernardo.enock@vileve.com.br>
2025-09-06 17:34:11 -0300

    feat: Create PostService

---
+++ argonock/src/main/java/com/api/argonock/service/PostService.java
@@ -0,0 +1,53 @@
+package com.api.argonock.service;
+
+import com.api.argonock.dto.PostRequestDTO;
+import com.api.argonock.model.Post;
+import com.api.argonock.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+  @Autowired
+  private PostRepository postRepository;
+
+  public List<Post> findAllPosts() {
+    return postRepository.findAll();
+  }
+
+  public Optional<Post> findPostById(Long id) {
+    return postRepository.findById(id);
+  }
+
+  public Post createPost(Post post) {
+    return postRepository.save(post);
+  }
+
+  public Post updatePost(Long id, PostRequestDTO requestDTO) {
+    Post post = postRepository.findById(id)
+        .orElseThrow(() -> new RuntimeException("Post not found for this id :: " + id));
+
+    post.setTitle(requestDTO.getTitle());
+    post.setBody(requestDTO.getBody());
+
+    return postRepository.save(post);
+  }
+
+  public void deletePost(Long id) {
+    postRepository.deleteById(id);
+  }
+
+  public List<Post> findPostsByUserId(String userId) {
+    return postRepository.findByUserId(userId);
+  }
+
+  public boolean isPostOwner(Long postId, String userId) {
+    return postRepository.findById(postId)
+        .map(post -> post.getUser().getId().equals(userId))
+        .orElse(false);
+  }
+}
