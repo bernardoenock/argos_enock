8fed19c56c4b9a7d8c21711f2486f60607d1cf11
Enock <bernardo.enock@vileve.com.br>
2025-09-08 16:56:18 -0300

    fix: AuthorizationService

--- argonock/src/main/java/com/api/argonock/service/AuthorizationService.java
+++ argonock/src/main/java/com/api/argonock/service/AuthorizationService.java
@@ -3,11 +3,14 @@ package com.api.argonock.service;
 import com.api.argonock.model.User;
 import com.api.argonock.model.UserRole;
 import com.api.argonock.repository.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Collection;
 
 @Service
 public class AuthorizationService implements UserDetailsService {
@@ -22,16 +25,66 @@ public class AuthorizationService implements UserDetailsService {
 
   @Override
   public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-    UserDetails user = userRepository.findByEmail(email);
-    if (user == null) {
-      throw new UsernameNotFoundException("User not found with email: " + email);
-    }
-    return user;
+    User user = findByEmail(email);
+    return new CustomUserDetails(user);
   }
 
-  public void saveUser(String email, String password, UserRole role) {
+  public User findByEmail(String email) {
+    return userRepository.findByEmail(email)
+        .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+  }
+
+  public User saveUser(String email, String password, UserRole role) {
     String encryptedPassword = passwordEncoder.encode(password);
     User newUser = new User(email, encryptedPassword, role);
-    userRepository.save(newUser);
+    return userRepository.saveAndFlush(newUser);
+  }
+
+  public static class CustomUserDetails implements UserDetails {
+
+    private final User user;
+
+    public CustomUserDetails(User user) {
+      this.user = user;
+    }
+
+    public User getUser() {
+      return user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+      return user.getAuthorities();
+    }
+
+    @Override
+    public String getPassword() {
+      return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+      return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+      return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+      return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+      return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+      return true;
+    }
   }
 }
