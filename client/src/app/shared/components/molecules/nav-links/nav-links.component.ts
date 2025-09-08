5f4b2e75f32535c95adc929498db3af3351c42eb
Enock <bernardo.enock@vileve.com.br>
2025-09-08 18:26:54 -0300

    feat: Create component Nav Links

---
+++ client/src/app/shared/components/molecules/nav-links/nav-links.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-nav-links',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './nav-links.component.html',
+  styleUrls: ['./nav-links.component.css']
+})
+export class NavLinksComponent {}
