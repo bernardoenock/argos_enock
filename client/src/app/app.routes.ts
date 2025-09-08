2aa8b5afae9878279d3b22df3e70c591364526fb
Enock <bernardo.enock@vileve.com.br>
2025-09-08 17:03:51 -0300

    feat: Define routers

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,40 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent)
+  },
+  {
+    path: 'auth/login',
+    loadComponent: () => import('./pages/auth/login/login.component').then(m => m.LoginComponent)
+  },
+  {
+    path: 'auth/register',
+    loadComponent: () => import('./pages/auth/register/register.component').then(m => m.RegisterComponent)
+  },
+  {
+    path: 'posts',
+    loadComponent: () => import('./pages/posts/posts-list/posts-list.component').then(m => m.PostsListComponent)
+  },
+  {
+    path: 'posts/:id',
+    loadComponent: () => import('./pages/posts/post-detail/post-detail.component').then(m => m.PostDetailComponent)
+  },
+  {
+    path: 'albums',
+    loadComponent: () => import('./pages/albums/albums-list/albums-list.component').then(m => m.AlbumsListComponent)
+  },
+  {
+    path: 'albums/:id',
+    loadComponent: () => import('./pages/albums/album-detail/album-detail.component').then(m => m.AlbumDetailComponent)
+  },
+  {
+    path: 'admin',
+    loadComponent: () => import('./pages/admin/admin.component').then(m => m.AdminComponent)
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
